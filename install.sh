8af593381a43b915c82afaed6ab0cdd47c39b166
Martin Christoph Frank <martinchristophfrank@gmail.com>
2023-11-08 21:08:39 +0100

    seems to work for now

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@
 REPO_NAME="toolbuilder"
 REPO_URL="https://github.com/m-c-frank/$REPO_NAME.git"
 INSTALL_PATH="$HOME/.local/bin"
-CLONE_PATH="./$REPO_NAME"
-
+CLONE_PATH="$HOME/$REPO_NAME"
 
 # Update package database and install Git and Go if they are not already installed
 if ! command -v git &>/dev/null; then
@@ -19,24 +18,36 @@ if ! command -v go &>/dev/null; then
 fi
 
 # Clone the ToolBuilder repository.
-git clone "$REPO_URL" "$CLONE_PATH" 
+if [[ -d "$CLONE_PATH" ]]; then
+    echo "Cleaning up old repository directory..."
+    rm -rf "$CLONE_PATH"
+fi
+
+echo "Cloning the ToolBuilder repository..."
+git clone "$REPO_URL" "$CLONE_PATH"
 
 # Change to the repository directory.
-cd "$CLONE_PATH" || exit
+cd "$CLONE_PATH" || { echo "Cloning failed, directory not found."; exit 1; }
+
+# Initialize Go module and tidy up dependencies.
+echo "Initializing Go module..."
+go mod init "$REPO_NAME"
+go mod tidy
 
 # Compile the Go source code.
-go build -o "$REPO_NAME" toolbuilder.go
+echo "Building the ToolBuilder..."
+go build -o "$REPO_NAME" .
 
 # Make the directory if it doesn't exist.
 mkdir -p "$INSTALL_PATH"
 
 # Install the binary.
+echo "Installing the ToolBuilder binary..."
 mv "$REPO_NAME" "$INSTALL_PATH"
 
 # Clean up the repository directory.
-if [[ -d "$CLONE_PATH" ]]; then
-    rm -rf "$CLONE_PATH"
-fi
+echo "Cleaning up..."
+rm -rf "$CLONE_PATH"
 
 echo "ToolBuilder installed successfully."
 
